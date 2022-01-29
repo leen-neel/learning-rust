f331db9245d997b4a5978d0696f5956790a6e959
leen-neel <and24903@gmail.com>
2022-01-29 12:46:26 +0530

    more variables!

--- variables.rs
+++ variables.rs
@@ -5,5 +5,13 @@ fn main() {
 
     let a = 2;
     let b = 2;
-    println!("{0} + {1} = {2}", a, b, a + b)
+    println!("{0} + {1} = {2}", a, b, a + b);
+
+    let mut mutable = "I can be changed!";
+    println!("{}", mutable);
+    mutable = "I got changed!";
+    println!("{}", mutable);
+
+    let (course, category) = ("Rust", "beginner");
+    println!("This is a {0} course in {1}", category, course);
 }

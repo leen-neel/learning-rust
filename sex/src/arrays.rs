8fab3f4cd5638c8b2f79e8814c1915fac259bf2b
leen-neel <and24903@gmail.com>
2023-08-17 21:52:12 +0530

    whoa

--- sex/src/arrays.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    
-    let arr1:[i32;5] = [1, 2, 3, 4, 5];
-    println!("{}", arr1[0]);
-    
-    let slice_arr:&[i32] = &arr1[0..2];
-    println!("{:?}", slice_arr)
-
-}
--- sex/src/hello.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("Hello world");
-    println!("Hi this is a {}", "test");
-    println!("This is a {1}, but this {0}", "isn't", "test");
-    eprint!("This is an error");
-}
